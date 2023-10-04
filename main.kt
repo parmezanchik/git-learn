906cf1a899f941d13be6b368661dcb6c0241c766
Cilb <maximvitokhin@gmail.com>
2023-10-04 17:22:17 +0300

    Ukrainianization main

--- main.kt
+++ main.kt
@@ -2,6 +2,6 @@ fun main() {
     val order1 = Order(1, 2)
     val order2 = Order(2, 3)
 
-    println(order1)  // Вывод информации о заказе 1
-    println(order2)  // Вывод информации о заказе 2
+    println(order1)  // Виведення інформації про замовлення 1
+    println(order2)  // Виведення інформації про замовлення 2
 }
